9f272310b45526c8fa75ba2ed3cda2d646650ac3
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-12 09:07:59 -0300

    081224 1

--- escolha.js
+++ escolha.js
@@ -18,25 +18,3 @@ switch(bebida){
         console.log(`Essa ${bebida} não é uma opção! Escolha entre café, leite ou chá.`);
 }
 console.log(`Você escolheu ${bebida} e o seu valor é ${valor.toFixed(2)}!`);
-
-/*
-let bebida = null;
-let valor = "0";
-let valorFloat = parseFloat(valor);
-let valorFormatado = valorFloat.toFixed(2);
- 
-switch(bebida){
-    case "café":
-        break;
-
-    case "leite":
-        break;
-
-    case "chá":
-        break;
-
-    default:
-        console.log(`Essa ${bebida} não é uma opção! Escolha entre café, leite ou chá.`);
-}
-console.log(`Você escolheu ${bebida} e o seu valor é ${valorFormatado}!`);
-*/
